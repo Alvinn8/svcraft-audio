96680cc1b35d1cccbc84e816250e457768e428fd
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2022-03-15 19:59:31 +0100

    Fix two websockets on one http server

--- server/server.js
+++ server/server.js
@@ -3,8 +3,7 @@ const http = require("http");
 const ws = require("ws");
 const { ExpressPeerServer } = require("peer");
 
-const WEB_PORT = process.env.WEB_PORT || 8000;
-const WS_PORT = process.env.WS_PORT || 8001;
+const PORT = process.env.WEB_PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
@@ -20,12 +19,29 @@ const peerServer = ExpressPeerServer(server, {
 
 app.use(peerServer);
 
+// Remove the upgrade listener, because we are running two websockets on the same
+// http server we need to handle upgrades manually and forward it to the correct
+// websocket. See below.
+let peerWebSocketListener;
+for (const func of server.listeners("upgrade")) {
+    server.removeListener("upgrade", func);
+    peerWebSocketListener = func;
+}
+
 // === Static HTTP Server ===
 
 app.use(express.static(__dirname));
 
 app.get("/", (request, response) => {
     response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/test", (request, response) => {
+    response.statusCode = 426;
+    response.contentType = "text/plain";
+    const body = http.STATUS_CODES[426];
+
+    response.end(body);
 });
 
 // === Web Socket ===
@@ -41,14 +57,19 @@ websocketServer.on("connection", function(ws) {
 });
 
 server.on("upgrade", function(request, socket, head) {
-    console.log("got upgrade");
-    websocketServer.handleUpgrade(request, socket, head, function(socket) {
-        console.log("done");
-        websocketServer.emit('connection', socket, request);
-    });
+    if (request.url && request.url.startsWith("/peer")) {
+        // A connection to the peer, let the peer server handle the request
+        peerWebSocketListener(request, socket, head);
+    } else {
+        // A connection to the svcraft-audio websocket, let the websocketServer
+        // handle the request.
+        websocketServer.handleUpgrade(request, socket, head, function(socket) {
+            websocketServer.emit("connection", socket, request);
+        });
+    }
 });
 
 // Start server
 
-server.listen(WEB_PORT);
-console.log("Server listening on port " + WEB_PORT);
+server.listen(PORT);
+console.log("Server listening on port " + PORT);
